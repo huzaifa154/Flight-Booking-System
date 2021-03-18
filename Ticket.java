20044603ff76c079e565ab9fc7a2b47aaa499c29
Huzaifa Ali <57848119+huzaifa154@users.noreply.github.com>
2021-03-17 23:47:49 -0400

    Create Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Huzaifa Ali
+ * 500901727
+ * TA: Sunbal Cheema
+ * Section: 11
+ */
+
+public class Ticket {
+ //Instance Variables  
+    private Passenger passenger;
+    private Flight flight;
+    double price;
+    private static int ticketNumber =0;
+    
+    //Constructor 
+    public Ticket (Passenger p, Flight flight, double price) {
+        this.passenger = p;
+        this.flight = flight;
+        this.price = price;
+    }
+  
+// getters and setters for each instance variable
+    public Passenger getPassenger() {
+        return this.passenger;
+    }
+    
+    public void setPassenger (Passenger p) {
+        this.passenger = p;
+    }
+
+    public Flight getFlight() {
+        return this.flight;
+    }
+    
+    public void setFlight (Flight flight) {
+        this.flight = flight;
+    }
+    
+    public double getPrice() {
+        return this.price;
+    }
+    
+    public void setPrice (double price) {
+        this.price = price;
+    }
+    
+
+    @Override
+    public String toString() {
+        return getPassenger().getName() + ", " + flight.toString() + ",ticket Price: " + getPrice();
+    }
+}
