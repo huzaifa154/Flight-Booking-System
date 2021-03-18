86f69be13107570a8923814d8e2308e0d12ed8d9
Huzaifa Ali <57848119+huzaifa154@users.noreply.github.com>
2021-03-18 16:13:47 -0400

    Update FlightTest.java

--- FlightTest.java
+++ FlightTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab1;
 
 import org.junit.After;
@@ -12,13 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Huzaifa Ali
- * 500901727
- * TA: Sunbal Cheema
- * Section: 11
- */
 public class FlightTest {
     
     public FlightTest() {
