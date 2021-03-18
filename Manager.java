f5a2a73bd867cbcdcfe78f81637d847f6933c246
Huzaifa Ali <57848119+huzaifa154@users.noreply.github.com>
2021-03-18 16:14:10 -0400

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab1;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-/**
- *
- * @author Huzaifa Ali
- * 500901727
- * TA: Sunbal Cheema
- * Section: 11
- */
 
 public class Manager {
 
