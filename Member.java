21a5df5b9e3cde60711973e3345357f70924d4fd
Huzaifa Ali <57848119+huzaifa154@users.noreply.github.com>
2021-03-18 16:14:29 -0400

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,17 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab1;
 
-/**
- *
- * @author Huzaifa Ali
- * 500901727
- * TA: Sunbal Cheema
- * Section: 11
- */
 public class Member extends Passenger{
     //Instance Variable
         public int  yearsOfMembership;
